fb838d1771c83a802bb516e34a8da5a709b29dd7
Kirin Sharma <ksbav@Mac.bellarmine.edu>
2025-11-18 14:22:12 -0500

    Added documentation

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ class Test:
     def __init__(self, name):
         self.name = name
 
+    # This method returns a greeting message using the name attribute.
     def greet(self):
         return f"Hello, {self.name}!"
